906f6a65b0fe5650c801274ce6878f3165166817
Larry McCartin <larry.mccartin@oregon.gov>
2021-04-02 08:57:20 -0700

    Initial creation

---
+++ LoggingSupport/EventTypeEnricher.cs
@@ -0,0 +1,40 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DASIT.LoggingSupport
+{
+    public class EventTypeEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var numericHash = GetDeterministicHashCode(logEvent.MessageTemplate.Text);
+            var eventId = propertyFactory.CreateProperty("EventType", numericHash);
+            logEvent.AddOrUpdateProperty(eventId);
+        }
+
+        static int GetDeterministicHashCode(string str)
+        {
+            unchecked
+            {
+                int hash1 = (5381 << 16) + 5381;
+                int hash2 = hash1;
+
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1)
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+                }
+
+                return hash1 + (hash2 * 1566083941);
+            }
+        }
+
+    }
+
+}
